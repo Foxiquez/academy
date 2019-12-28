ba8ba8f4c5f9de8da7bec794070d3aef6bfe90bd
Foxiquez <foxiquez.bl@gmail.com>
2019-12-28 18:02:53 +0300

    Refactoring db statuses & make composer provider

--- app/User.php
+++ app/User.php
@@ -11,13 +11,23 @@ class User extends Authenticatable
 {
     use Notifiable, SoftDeletes;
 
+    protected const ROLE_STUDENT = 0;
+    protected const ROLE_TEACHER = 1;
+    protected const ROLE_ADMIN = 2;
+
+
+    protected const NEW_USER_STATUS = 0;
+    protected const ACTIVE_STATUS = 1;
+    protected const FREEZED_STATUS = 2;
+    protected const BANNED_STATUS = 3;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'login', 'email', 'password',
+        'login', 'email', 'password', 'role'
     ];
 
     /**
@@ -38,13 +48,33 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function getStatusAttribute()
+    //// Check roles;
+    public function isTeacher(): bool
     {
-        return $this->statuses->first();
+        return $this->role === self::ROLE_TEACHER;
     }
 
-    public function statuses()
+    public function isAdmin(): bool
     {
-        return $this->belongsToMany(Status::class);
+        return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isStudent(): bool
+    {
+        return $this->role === self::ROLE_STUDENT;
+    }
+
+    //// Check statuses;
+    public function isActive(): bool
+    {
+        return $this->status === self::ROLE_STUDENT;
+    }
+    public function isFreezed(): bool
+    {
+        return $this->status === self::ROLE_STUDENT;
+    }
+    public function isBanned(): bool
+    {
+        return $this->status === self::ROLE_STUDENT;
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,7 +8,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
-            StatusesTableSeeder::class,
         ]);
 
     }
